b3f7d5ad45551d1b105aedee4abc0b7de0ba56e7
Eric Yang <eric8yang@gmail.com>
2021-05-18 18:04:32 -0700

    5/16/2021

---
+++ 164.py
@@ -0,0 +1,17 @@
+#seems wrong but it got accepted
+class Solution:
+    def maximumGap(self, nums) -> int:
+        if len(nums) < 2:
+            return 0
+        from sortedcontainers import SortedSet
+        s = SortedSet([])
+        for i in nums:
+            s.add(i)
+        diff = 0
+        prev = s[0]
+        for i in s[1:]:
+            temp = i - prev
+            diff = temp if temp > diff else diff
+            prev = i
+        return diff
+        
