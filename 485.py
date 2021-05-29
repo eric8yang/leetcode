0d95976aab0f45521c10f28e9330ec996f99e310
Eric Yang <eric8yang@gmail.com>
2021-05-29 00:11:06 -0700

    5/29/2021

--- 485.py
+++ 485.py
@@ -16,3 +16,15 @@ class Solution:
         
         return greatest
             
+#better solution without using sliding window:
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        count = 0
+        maxL = 0
+        for num in nums:
+            if num == 0:
+                maxL = max(count, maxL)
+                count = 0
+            else:
+                count+=1
+        return max(maxL, count)
